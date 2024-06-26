cd887bc01a5a5e46977bafa7d305b5df596813db
saiprasad2412 <manesaiprasad2412@gmail.com>
2024-06-26 18:31:47 +0530

    remove cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app= express();
 dotenv.config({
     path:'./.env'
 })
-app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
+app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/v1/posts',postRoute)
